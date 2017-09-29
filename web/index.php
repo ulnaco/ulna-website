8970a9ce177f6061ad5699a30dd19788421cc8db
Ulna <hello@ulna.co>
2017-09-29 11:26:23 -0500

    Proof read

--- web/index.php
+++ web/index.php
@@ -36,7 +36,7 @@
 				</header>
 				<p class="content">
 					<span class="image left"><img src="assets/images/anatomy-logo.png" alt="Ulna Anatomy iOS App"></span>
-					Ulna Anatomy is an iOS App that uses Apple Health to help you improve your health. <i>How healthy are you? How would you rate your Health?</i> Ulna Anatomy creates a health rating based on your weight and fitness. Inside the app, you can also log your weight and share that data across other applications. It's possible to dramatically optimize people’s health and efficiency with well-built software that helps them reach their true potential. Ulna Anatomy is free and currently in beta. Ulna Anatomy launches Fall of 2017 on iOS. Join the public Beta today.</p>
+					Ulna Anatomy is an iOS App that uses Apple Health to improve your health. <i>How healthy are you? How would you rate your Health?</i> Ulna Anatomy creates a health rating based on your weight and fitness. Inside the app, you can also log your weight and share that data across other applications. It's possible to dramatically optimize people’s health and efficiency with well-built software that helps them reach their true potential. Ulna Anatomy is free and currently in beta. Ulna Anatomy launches Fall of 2017 on iOS. Join the public Beta today.</p>
 
 					<img src="assets/images/screenshots.png" width="100%" />
 
@@ -48,7 +48,7 @@
 						#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
 					</style>
 					<div id="mc_embed_signup">
-					<form action="//ulna.us16.list-manage.com/subscribe/post?u=c8d0a1ba572e7dcab5e14750c&amp;id=67170d2aa2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<form action="//ulna.us16.list-manage.com/subscribe/post?u=c8d0a1ba572e7dcab5e14750c&amp;id=67170d2aa2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" style="padding: 0" novalidate>
 			    	<div id="mc_embed_signup_scroll">
 							<h2>Join the Beta</h2>
 							<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
